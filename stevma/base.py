d8a7833343f3bcea0c90b89bed41c3782d7fae37
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2022-05-26 15:34:42 -0300

    add function that computes the number of points in the meshgrid

--- stevma/base.py
+++ stevma/base.py
@@ -7,8 +7,6 @@ import sys
 
 import pprint
 
-from stevma.mesa import _defaultStarNamelists, _defaultBinaryNamelists
-from stevma.mesa.mesa import get_mesa_defaults
 from stevma.io.io import load_yaml
 from stevma.io.logger import logger
 
@@ -143,25 +141,74 @@ class Manager(object):
     def set_meshgrid(self) -> None:
         """Create grid of evolutionary models"""
 
+        def check_for_valid_namelist_options(d: dict = {}) -> bool:
+            """Function that checks whether a dictionary contains valid namelists of MESA as well
+            as valid name of options
+
+            Parameters
+            ----------
+            d : `dict`
+                Dictionary with MESA options for the meshgrid
+            """
+
+            # import some info from mesa module
+            from stevma.mesa import _defaultStarNamelists, _defaultBinaryNamelists
+            from stevma.mesa.mesa import get_mesa_defaults
+
+            # need MESA defaults parameters to check whether the arguments of the meshgrid are valid
+            _MESADefaults = get_mesa_defaults(mesa_dir=os.environ["MESA_DIR"])
+
+            # check that each key in the dict grid is actually a valid namelist
+            namelists = [namelist for namelist in _defaultStarNamelists]
+            namelists.extend(([namelist for namelist in _defaultBinaryNamelists]))
+
+            is_okay = True
+            for key in d.keys():
+                if key not in namelists:
+                    logger.critical(f"namelist `{key}` not present in MESA source code")
+                    is_okay = False
+                    break
+                else:
+                    tmpDict = d[key]
+                    for subkey in tmpDict.keys():
+                        if subkey not in _MESADefaults.get(key):
+                            logger.critical(
+                                f"option `{subkey}` not valid (not found in MESA defaults)"
+                            )
+                            is_okay = False
+                            break
+
+            return is_okay
+
+        def get_number_of_gridpoints(d: dict = {}) -> int:
+            """Get the number of points in the meshgrid
+
+            Parameters
+            ----------
+            d : `dict`
+                Dictionary with meshgrid points
+            """
+            n = 1
+
+            # we assume that there dictionary has one key with the namelist of MESA
+            # which is in itself another dictionary with the actual gridpoints
+            for namelist in d.keys():
+                options = d[namelist]
+                for option in options.keys():
+                    if isinstance(options[option], list):
+                        n *= len(options[option])
+                    else:
+                        logger.debug(f"{option} contains only one element")
+
+            return n
+
         # get dict of parameters that will be changing in the grid, each key of the dict
         # corresponds to a certain namelist of the MESA source code
         model_grid = self._load_meshgrid()
 
-        # need MESA defaults parameters to check whether the arguments of the meshgrid are valid
-        _MESADefaults = get_mesa_defaults(mesa_dir=os.environ["MESA_DIR"])
+        # check of valid options in meshgrid
+        if not check_for_valid_namelist_options(d=model_grid):
+            sys.exit(1)
 
-        # check that each key in the dict grid is actually a valid namelist
-        namelists = [namelist for namelist in _defaultStarNamelists]
-        namelists.extend(([namelist for namelist in _defaultBinaryNamelists]))
-        for key in model_grid.keys():
-            if key not in namelists:
-                logger.critical(f"namelist `{key}` not present in MESA source code")
-                sys.exit(1)
-            else:
-                tmpDict = model_grid[key]
-                for subkey in tmpDict.keys():
-                    if subkey not in _MESADefaults.get(key):
-                        logger.critical(
-                            f"option `{subkey}` not valid (not found in MESA defaults)"
-                        )
-                        sys.exit(1)
+        number_of_gridpoints = get_number_of_gridpoints(d=model_grid)
+        print(number_of_gridpoints)
