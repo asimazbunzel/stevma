3e68f53e28517168cdd1d03f0e787cf7cbb3c8b4
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2022-05-26 19:55:21 -0300

    add a check at the beginning of the Manager for the MESA_DIR variable

--- stevma/base.py
+++ stevma/base.py
@@ -9,6 +9,7 @@ import pprint
 
 from stevma.io.io import load_yaml
 from stevma.io.logger import logger
+from stevma.meshgrid import check_for_valid_namelist_options, get_number_of_gridpoints
 
 
 class Manager(object):
@@ -38,6 +39,22 @@ class Manager(object):
 
         # load config
         self.config = self.load_config_file()
+
+        # always get MESA_DIR value
+        mesa_dir = self.config["mesa"].get("mesa_dir")
+        if mesa_dir == "" or mesa_dir is None:
+            mesa_dir = os.environ.get("MESA_DIR")
+            if mesa_dir == "" or mesa_dir is None:
+                logger.critical(
+                    "mesa_dir must be defined either in the config file or as environment variable"
+                )
+                sys.exit(1)
+
+        # always use pathlib
+        if isinstance(mesa_dir, str):
+            self.mesa_dir = Path(mesa_dir)
+        else:
+            self.mesa_dir = mesa_dir
 
         # load mesh of stellar evolution models
         self.meshgrid = None
@@ -141,73 +158,12 @@ class Manager(object):
     def set_meshgrid(self) -> None:
         """Create grid of evolutionary models"""
 
-        def check_for_valid_namelist_options(d: dict = {}) -> bool:
-            """Function that checks whether a dictionary contains valid namelists of MESA as well
-            as valid name of options
-
-            Parameters
-            ----------
-            d : `dict`
-                Dictionary with MESA options for the meshgrid
-            """
-
-            # import some info from mesa module
-            from stevma.mesa import _defaultStarNamelists, _defaultBinaryNamelists
-            from stevma.mesa.mesa import get_mesa_defaults
-
-            # need MESA defaults parameters to check whether the arguments of the meshgrid are valid
-            _MESADefaults = get_mesa_defaults(mesa_dir=os.environ["MESA_DIR"])
-
-            # check that each key in the dict grid is actually a valid namelist
-            namelists = [namelist for namelist in _defaultStarNamelists]
-            namelists.extend(([namelist for namelist in _defaultBinaryNamelists]))
-
-            is_okay = True
-            for key in d.keys():
-                if key not in namelists:
-                    logger.critical(f"namelist `{key}` not present in MESA source code")
-                    is_okay = False
-                    break
-                else:
-                    tmpDict = d[key]
-                    for subkey in tmpDict.keys():
-                        if subkey not in _MESADefaults.get(key):
-                            logger.critical(
-                                f"option `{subkey}` not valid (not found in MESA defaults)"
-                            )
-                            is_okay = False
-                            break
-
-            return is_okay
-
-        def get_number_of_gridpoints(d: dict = {}) -> int:
-            """Get the number of points in the meshgrid
-
-            Parameters
-            ----------
-            d : `dict`
-                Dictionary with meshgrid points
-            """
-            n = 1
-
-            # we assume that there dictionary has one key with the namelist of MESA
-            # which is in itself another dictionary with the actual gridpoints
-            for namelist in d.keys():
-                options = d[namelist]
-                for option in options.keys():
-                    if isinstance(options[option], list):
-                        n *= len(options[option])
-                    else:
-                        logger.debug(f"{option} contains only one element")
-
-            return n
-
         # get dict of parameters that will be changing in the grid, each key of the dict
         # corresponds to a certain namelist of the MESA source code
         model_grid = self._load_meshgrid()
 
         # check of valid options in meshgrid
-        if not check_for_valid_namelist_options(d=model_grid):
+        if not check_for_valid_namelist_options(d=model_grid, mesa_dir=self.mesa_dir):
             sys.exit(1)
 
         number_of_gridpoints = get_number_of_gridpoints(d=model_grid)
